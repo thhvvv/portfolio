97496ed1de0460c510bc50f57110a8bc2ec0c2c3
thhvvv <makausilvia964@gmail.com>
2024-10-23 01:43:16 +0300

    test

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = '15259ef27820e177915cc1cbc29f0059'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///agritech.db'  # SQLite for simplicity
     SQLALCHEMY_TRACK_MODIFICATIONS = False
